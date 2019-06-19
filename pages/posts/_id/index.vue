9df56ead0f3c7661e11532c27e5fb4637c29f5d3
iMacAB <bagga.abdelbasset@gmail.com>
2019-06-19 18:27:08 +0100

    Create project & Adding External Fonts & Creating the Landing Page

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="single-post-page">
+        <section class="post">
+            <h1 class="post-title">Title of the Post</h1>
+            <div class="post-details">
+                <div class="post-detail">Last updated on XXX</div>
+                <div class="post-detail">Written by NAME</div>
+            </div>
+            <p>  Content of the post </p>
+        </section>
+        <section class="post-feedback">
+            <p>Let me know what you think about the post, Send a mail to <a href="mailto:test@test.com">test@test.com</a></p>
+        </section>
+    </div>
+</template>
