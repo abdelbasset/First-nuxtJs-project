5e0a2a73e2b161c5de1a5332e93986837c0c97f6
iMacAB <bagga.abdelbasset@gmail.com>
2019-09-04 20:18:46 +0100

    Add UI Component & Add form create post

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <PostPreview
+        id="1"
+        thumbnail="https://702rc195vu6fsa4x3pnb6d8m-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
+        title="Hello There"
+        textPreview="Hello Preview test"
+      />
+      <PostPreview
+        id="2"
+        thumbnail="https://702rc195vu6fsa4x3pnb6d8m-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
+        title="Hello There 2"
+        textPreview="Hello Preview test"
+      />
+      <PostPreview
+        id="3"
+        thumbnail="https://702rc195vu6fsa4x3pnb6d8m-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
+        title="Hello There 3"
+        textPreview="Hello Preview test"
+      />
+    </section>
+</template>
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+export default {
+    components:{
+        PostPreview
+    }
+    
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle ="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
     <nuxt />
   </div>
 </template>
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSidenav from '@/components/Navigation/TheSidenav';
+import TheSideNavToggle from '@/components/Navigation/TheSideNavToggle';
+
+export default {
+  data() {
+    return {
+      displaySidenav: false
+    }
+  },
+  components : {
+      TheHeader,
+      TheSidenav,
+      TheSideNavToggle
+  }
+}
+</script>
 
 <style>
 html{
