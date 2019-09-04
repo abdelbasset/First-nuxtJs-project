0e91f46ad6ced1564fbc146c25796b332d00f6c2
iMacAB <bagga.abdelbasset@gmail.com>
2019-09-04 19:40:13 +0100

    Add ListPost Component

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,14 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-        id="1"
-        thumbnail="https://702rc195vu6fsa4x3pnb6d8m-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
-        title="Hello There"
-        textPreview="Hello Preview test"
-      />
-      <PostPreview
-        id="2"
-        thumbnail="https://702rc195vu6fsa4x3pnb6d8m-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
-        title="Hello There 2"
-        textPreview="Hello Preview test"
-      />
-      <PostPreview
-        id="3"
-        thumbnail="https://702rc195vu6fsa4x3pnb6d8m-wpengine.netdna-ssl.com/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
-        title="Hello There 3"
-        textPreview="Hello Preview test"
-      />
-    </section>
+      <PostList />
   </div>
 </template>
 <script>
-import PostPreview from "@/components/Posts/PostPreview";
+import PostList from "@/components/Posts/PostList";
 export default {
   components: {
-    PostPreview
+    PostList
   }
 };
 </script>
